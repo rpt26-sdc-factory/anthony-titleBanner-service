c3f264c2d310c5f1c59a633b54c9bd1d29e243cb
Anthony Sim <anthonyysim@gmail.com>
2021-04-29 18:34:35 -0700

    Update / PUT - update an item

--- server/controllers/crudControllers.js
+++ server/controllers/crudControllers.js
@@ -2,6 +2,7 @@ const Title = require('../../db/title.model');
 const axios = require('axios');
 const mongoose = require('mongoose');
 
+
 exports.postTitle = async (req, res) => {
   Title.getCollection('titles').find({}, (err, results) => {
     if (err) {
@@ -13,6 +14,25 @@ exports.postTitle = async (req, res) => {
   res.end();
   //   let { title } = req.body;
   //   res.send({ message: title });
+};
+
+// Update / PUT - update an item
+exports.putTitle = async (req, res) => {
+  await mongoose.connect('mongodb://localhost/titleDB', { useUnifiedTopology: true, useNewUrlParser: true });
+
+  Title.updateOne({ title: req.params.title }, { title: req.body.title }, (err, result) => {
+    if (err) {
+      console.error(err);
+
+    } else {
+      if (!result) {
+        console.log(`"${req.params.title}" does NOT exist in database!`);
+
+      } else {
+        res.json({ message: `Title: ${req.params.title}, UPDATED to Title: ${req.body.title} from the database!` })
+      }
+    }
+  })
 };
 
 // Read / GET - read an item
@@ -34,11 +54,6 @@ exports.getTitle = async (req, res) => {
   })
 };
 
-
-exports.putTitle = (req, res) => {
-
-};
-
 // Delete / DELETE - delete an item
 exports.deleteTitle = async (req, res) => {
   await mongoose.connect('mongodb://localhost/titleDB', { useUnifiedTopology: true, useNewUrlParser: true });
@@ -52,8 +67,7 @@ exports.deleteTitle = async (req, res) => {
         console.log(`"${req.params.title}" does NOT exist in database!`);
 
       } else {
-        console.log(`"${req.params.title}" has been DELETED from the database!`);
-        res.json({ message: 'fuck u' })
+        res.json({ message: `Title: ${req.params.title}, DELETED from the database!` })
       }
     }
   })
