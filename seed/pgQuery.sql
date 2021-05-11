ea789797402cda70355db9087e66ffea2bcb55d9
Anthony Sim <anthonyysim@gmail.com>
2021-05-10 23:45:31 -0700

    updated to pqQuery.sql

--- seed/pgQuery.sql
+++ seed/pgQuery.sql
@@ -1,47 +1,37 @@
--- Start Time
-SELECT EXTRACT(MILLISECONDS FROM now());
-
 -- TITLES QUERIES
 \c titles_database
 
 -- Get the 10 millionth record
-SELECT * FROM titles WHERE id=10000000;
-SELECT EXTRACT(MILLISECONDS FROM now());
+EXPLAIN ANALYZE SELECT * FROM titles WHERE id=10000000;
 
 -- Get the 7.5 millionth record
-SELECT * FROM titles WHERE id=7500000;
-SELECT EXTRACT(MILLISECONDS FROM now());
+EXPLAIN ANALYZE SELECT * FROM titles WHERE id=7500000;
 
 -- Get the 5 millionth record
-SELECT * FROM titles WHERE id=5000000;
-SELECT EXTRACT(MILLISECONDS FROM now());
+EXPLAIN ANALYZE SELECT * FROM titles WHERE id=5000000;
 
 -- Get the 2.5 millionth record
-SELECT * FROM titles WHERE id=2500000;
-SELECT EXTRACT(MILLISECONDS FROM now());
+EXPLAIN ANALYZE SELECT * FROM titles WHERE id=2500000;
 
 
 -- ENROLLED QUERIES
 \c enrolled_database
 
 -- Get the 10 millionth record
-SELECT * FROM enrolled WHERE id=10000000;
-SELECT EXTRACT(MILLISECONDS FROM now());
+EXPLAIN ANALYZE SELECT * FROM enrolled WHERE id=10000000;
 
 -- Get the 7.5 millionth record
-SELECT * FROM enrolled WHERE id=7500000;
-SELECT EXTRACT(MILLISECONDS FROM now());
+EXPLAIN ANALYZE SELECT * FROM enrolled WHERE id=7500000;
 
 -- Get the 5 millionth record
-SELECT * FROM enrolled WHERE id=5000000;
-SELECT EXTRACT(MILLISECONDS FROM now());
+EXPLAIN ANALYZE SELECT * FROM enrolled WHERE id=5000000;
 
 -- Get the 2.5 millionth record
-SELECT * FROM enrolled WHERE id=2500000;
-SELECT EXTRACT(MILLISECONDS FROM now());
+EXPLAIN ANALYZE SELECT * FROM enrolled WHERE id=2500000;
 
 \c postgres
 
+-- run queries command: \i seed/pgQuery.sql
 
 
 
