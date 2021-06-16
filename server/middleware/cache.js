5e9c36248d5657ad1986bb60d6170629c6275226
Anthony Sim <anthonyysim@gmail.com>
2021-06-15 19:17:46 -0700

    redis npm

---
+++ server/middleware/cache.js
@@ -0,0 +1,19 @@
+const { client } = require('../redis-client/client');
+
+
+// Cache middleware
+exports.cache = (req, res, next) => {
+  const { id } = req.params;
+
+  client.get(id, (err, data) => {
+    if (err) throw err;
+
+    if (data !== null) {
+      console.log(data);
+      res.send(data);
+      next();
+    } else {
+      next();
+    }
+  });
+}
